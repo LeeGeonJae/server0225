82738c4ec78b300d149ae266589134c51ae0c83e
LeeeGeonJae <wnwkrdkemf@gmail.com>
2022-02-25 09:25:07 +0900

    server

---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <WinSock2.h>
+
+#pragma (lib, "wn32_2.lib")
+
+using namespace std;
+
+int main()
+{
+	WSADATA wsaData;
+
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData))
+	{
+		cout << "Winsock Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	
+	SOCKET ServerSocket;
+
+	ServerSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (ServerSocket == INVALID_SOCKET)
+	{
+		cout << "socket Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+
+	SOCKADDR_IN ServerAddr;
+
+	memset(&ServerAddr, 0, sizeof(ServerAddr));
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(50000);
+	ServerAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	if (bind(ServerSocket, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr)) == SOCKET_ERROR)
+	{
+		cout << "bind Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+
+	if (listen(ServerSocket, 0) == SOCKET_ERROR)
+	{
+		cout << "listen Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+
+	SOCKADDR_IN ClientAddr;
+	int ClientAddrSize = sizeof(ClientAddr);
+	SOCKET ClientSocket = 0;
+	ClientSocket = accept(ServerSocket, (SOCKADDR*)&ClientAddr, &ClientAddrSize);
+	if (ClientSocket == SOCKET_ERROR)
+	{
+		cout << "accept Error : " << GetLastError() << endl;
+		exit(-1);
+	}
+
+	char Message[] = "Hello World";
+	send(ClientSocket, Message, strlen(Message) + 1, 0);
+
+	closesocket(ClientSocket);
+	closesocket(ServerSocket);
+
+	WSACleanup();
+
+	return 0;
+}
